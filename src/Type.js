92debbe22d7f62e107f918699357a2b8b942d5b0
Vinh Ta <tavinh132@gmail.com>
2024-05-30 18:11:03 -0700

    typing makes shapes

--- src/Type.js
+++ src/Type.js
@@ -1,21 +1,45 @@
+// get room size in game
+// https://stackoverflow.com/questions/52139569/phaser-io-3-get-game-size-in-scene
+
 class Type extends Phaser.Scene {
     constructor() {
         super("typeScene");
     }
 
 preload() {
+    this.canvas = this.sys.game.canvas;
+
     this.load.audio("scream","./assets/scream1.mp3");
 }
 
     create(){
-        console.log("test");
-        backSpace = this.input.keyboard.addKey("BACKSPACE");   // adds backspace as a variable
+        let { width, height } = this.canvas;
+        // drawing circles
+        // https://labs.phaser.io/edit.html?src=src/geom\circle\circumference.js
+        const graphics = this.add.graphics({ lineStyle: { width: 3, color: 0xffffff }, fillStyle: { color: 0xfffffff } });
+        var randX = Phaser.Math.Between(0, width);
+        var randY = Phaser.Math.Between(0, height);
+
+        console.log(width + ", " + height);
+        backSpace = this.input.keyboard.addKey("BACKSPACE");    // adds backspace as a variable
         backSpace.on("down", () => {                            // checks for backspacing
             console.log("Deleting");
             this.sound.play("scream");
-        });                               
+        });
+        this.input.keyboard.on("keydown", (key) => {
+            if (!backSpace.isDown){                             // if any key is pressed other than backspace
+                // do this
+                var color = Phaser.Display.Color.RandomRGB().color;
+                graphics.fillStyle(color);
+                var circle = new Phaser.Geom.Circle(randX, randY, 5, color);
+                graphics.fillCircleShape(circle);
+                randX += Phaser.Math.Between(-3,3);
+                randY += Phaser.Math.Between(-3,3);
+            }
+        });                            
     }
 
     update(){
+
     }
 }
